58f80606647ce31e9b72839f43759fcb65b6a734
Soubhi Hadri <s.hadri@ou.edu>
2018-03-26 07:34:31 -0500

    Add files via upload

---
+++ _build/CMakeFiles/plc_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/plc_example.dir/main.cpp.o"
+  "plc_example.pdb"
+  "plc_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/plc_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
